4587f28c8fa14d01c5c0857f4caa3d26498f691d
max <max@10.99.94.194>
2014-12-20 13:48:58 -0500

    clean stack before eval hash

---
+++ src/test/scala/com/stack/issue/domain/StackTests.scala
@@ -0,0 +1,31 @@
+package com.stack.issue.domain
+
+import org.scalatra.test.scalatest.ScalatraSuite
+import org.scalatest.FunSuiteLike
+import org.scalatra.test.scalatest.ScalatraSpec
+
+final class StackTests extends ScalatraSuite with FunSuiteLike {
+
+  test("dates are removed") {
+    val stack = "balre Feb  29 07:59:59 balbla"
+
+    cleanedStack(stack) should equal("balre  balbla")
+  }
+
+  private def cleanedStack(stackToClean: String) = {
+    new Stack(stackToClean).clean.toString
+  }
+
+  test("uuid are removed") {
+    val stack = "reookk 21942148-236e-4300-97b1-1478907b770c ooqq"
+
+    cleanedStack(stack) should equal("reookk  ooqq")
+  }
+  
+  test("clean up is done"){
+    val stack = "bla Feb  29 07:59:59 and 21942148-236e-4300-97b1-1478907b770c but no"
+      
+      cleanedStack(stack) should equal("bla  and  but no")
+  }
+
+}
